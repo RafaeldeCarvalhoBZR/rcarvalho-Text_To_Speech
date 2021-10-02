8211bd9bb3ea5e22c70686bbfef7463912fd1bea
rcarvalho <rafael.carvalho@voxdatati.com.br>
2021-10-01 21:19:24 -0300

    1.0

---
+++ src/components/TextToSpeech.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card class=" bordered q-pa-md bg-grey-3 text-white">
+    <q-input class="q-pa-md" outlined v-model="textInput" label="Digite aqui" />
+    <div class=" q-pa-md">
+      <q-btn
+        class="q-px-md"
+        dense
+        no-caps
+        color="indigo"
+        id="play"
+        @click="speak()"
+      >
+        Falar
+      </q-btn>
+    </div>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "TextToSpeech",
+  data() {
+    return {
+      textInput: ""
+    };
+  },
+  methods: {
+    speak() {
+      let fala = new SpeechSynthesisUtterance(this.textInput);
+      window.speechSynthesis.speak(fala);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="bg-grey-5 flex flex-center">
+    <TextToSpeech />
+  </q-page>
+</template>
+
+<script>
+import TextToSpeech from "../components/TextToSpeech";
+
+export default {
+  name: "PageIndex",
+  components: {
+    TextToSpeech
+  },
+  created() {
+    console.log("PATH_TESTE", process.env.PATH_ROUTE);
+    console.log("URL_BASE", process.env.URL_BASE);
+  }
+};
+</script>
